d893268010ecf7450f178f796446d5aaa2b9d658
wilson espina <wilson.espina@gmail.com>
2024-10-30 16:30:33 +1300

    feat: adds validation checks

--- src/app/placeholder/placeholder.component.ts
+++ src/app/placeholder/placeholder.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+interface ISetAttributes {
+  dimensions: string;
+}
+
 @Component({
   selector: 'app-placeholder',
   standalone: true,
   imports: [],
   templateUrl: './placeholder.component.html',
-  styleUrl: './placeholder.component.css'
+  styleUrl: './placeholder.component.css',
 })
 export class PlaceholderComponent {
   dimensions!: any;
@@ -20,27 +24,49 @@ export class PlaceholderComponent {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-
+    this.route.paramMap.subscribe((params) => {
       this.dimensions = params.get('dimensions');
+      this.setAttributes({ dimensions: this.dimensions });
+    });
+  }
 
-      if (this.dimensions) { // do regex for 'x'
-        const [width, height] = this.dimensions.split('x').map(Number);
-        let customText = `${width}x${height}`; // Default text
-        let textColor = '#000000'; // Default text color
-        let backgroundColor = '#cccccc'; // Default background color
+  setAttributes({ dimensions }: ISetAttributes) {
+    if (dimensions && this.isValidDimension(dimensions)) {
+      // do regex for 'x'
+      const [width, height] = dimensions.split('x').map(Number);
+      let customText = `${width}x${height}`; // Default text
+      let textColor = '#000000'; // Default text color
+      let backgroundColor = '#cccccc'; // Default background color
 
-        const minDimension = Math.min(width, height);
-        const fontSize = minDimension * 0.1; // Font size as 5% of the smaller dimension
+      const minDimension = Math.min(width, height);
+      const fontSize = minDimension * 0.1; // Font size as 5% of the smaller dimension
 
-        this.width = String(width);
-        this.height = height;
-        this.customText = customText;
-        this.textColor = textColor;
-        this.backgroundColor = backgroundColor;
-        this.fontSize = fontSize.toString();
-        console.log("🚀 ~ PlaceholderComponent ~ ngOnInit ~ this.backgroundColor:", this.backgroundColor)
-      }
-    })
+      this.width = String(width);
+      this.height = String(height);
+      this.customText = customText;
+      this.textColor = textColor;
+      this.backgroundColor = backgroundColor;
+      this.fontSize = fontSize.toString();
+    }
+  }
+
+  isValidDimension(dimension?: string): boolean {
+    if (!dimension) return false;
+    // '300x400'
+    // ['300', 'x', '400']
+    const splitDimension = dimension.split(/(x)/);
+
+    return Boolean(
+      dimension?.includes('x') &&
+        splitDimension.length === 3 &&
+        this.isNumber(splitDimension[0]) &&
+        this.isNumber(splitDimension[2])
+    );
+  }
+
+  isNumber(string: string): boolean {
+    return !isNaN(Number(string));
   }
 }
+
+// https://github.dev/bick/placeholderjs
